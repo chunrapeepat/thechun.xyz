9e6ee1fd74d6cd216584d0649207424d1922c884
chunza2542 <chunza2542@hotmail.com>
2018-02-14 16:46:02 +0700

    ❤️ Happy valentine day

--- components/footer.js
+++ components/footer.js
@@ -31,7 +31,7 @@ export default class extends Component {
         <a href="https://facebook.com/chun42" target="_blank"><i className="zmdi zmdi-facebook-box"></i></a>
         <a href="https://github.com/chunza2542" target="_blank"><i className="zmdi zmdi-github"></i></a>
         <a href="https://www.linkedin.com/in/chun-r-0a67ab134/" target="_blank"><i className="zmdi zmdi-linkedin-box"></i></a>
-        <br/>Made by Chun Rapeepat, 2017.
+        <br/>Made with ❤️ by Chun Rapeepat, 2017.
       </Container>
     )
   }
