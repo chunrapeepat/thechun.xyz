97e0f16593dffe2e0f7b4df59e0d4a8f21619c91
chunza2542 <chunza2542@hotmail.com>
2018-06-25 21:11:38 +0700

    fix #10, update footer

--- components/footer.js
+++ components/footer.js
@@ -5,21 +5,20 @@ import { fontFamilyHeading } from '../core/styled'
 const Container = styled.div`
   width: calc(100% - 60px);
   display: block;
-  background: #444;
-  color: #ddd;
-  padding: 30px;
+  color: #666;
+  padding-bottom: 50px;
   font-family: sans-serif;
   text-align: center;
 
   ${this} > a {
     display: inline-block;
-    color: white;
+    color: #555;
     margin-bottom: 12px;
     font-size: 25px;
     transition: 0.2s;
     padding: 0 10px;
     &:hover {
-      color: white;
+      color: #111;
     }
   }
 `
@@ -31,7 +30,7 @@ export default class extends Component {
         <a href="https://facebook.com/chun42" target="_blank"><i className="zmdi zmdi-facebook-box"></i></a>
         <a href="https://github.com/chunza2542" target="_blank"><i className="zmdi zmdi-github"></i></a>
         <a href="https://www.linkedin.com/in/chun-r-0a67ab134/" target="_blank"><i className="zmdi zmdi-linkedin-box"></i></a>
-        <br/>Made with ❤️ by Chun Rapeepat, 2017.
+        <br/>Made with ❤️ by Chun Rapeepat, 2017 - 2018.
       </Container>
     )
   }
