ae0f906984ddf2985d9030780cc07b7bb97b6d9f
chunza2542 <chunza2542@hotmail.com>
2018-06-25 21:26:06 +0700

    add love toggle for footer

--- components/footer.js
+++ components/footer.js
@@ -23,14 +23,44 @@ const Container = styled.div`
   }
 `
 
+const Love = styled.i`
+  color: red;
+  margin: 0 7px;
+  cursor: pointer;
+  transform: scale(1.4);
+
+  &:hover {
+    transform: scale(1.6) rotate(-15deg);
+  }
+`
+
+const LoveSpan = styled.span`
+  margin-left: 5px;
+  margin-right: 8px;
+  color: #FE1386;
+  font-style: italic;
+  font-weight: bold;
+`
+
 export default class extends Component {
+  state = {
+    isText: false
+  }
   render(){
+    const state = this.state
     return(
       <Container>
         <a href="https://facebook.com/chun42" target="_blank"><i className="zmdi zmdi-facebook-box"></i></a>
         <a href="https://github.com/chunza2542" target="_blank"><i className="zmdi zmdi-github"></i></a>
         <a href="https://www.linkedin.com/in/chun-r-0a67ab134/" target="_blank"><i className="zmdi zmdi-linkedin-box"></i></a>
-        <br/>Made with ❤️ by Chun Rapeepat, 2017 - 2018.
+        <br/>Made with
+        {!state.isText &&
+          <Love onClick={() => this.setState({isText: true})} className="zmdi zmdi-favorite"/>
+        }
+        {state.isText &&
+          <LoveSpan>LOVE</LoveSpan>
+        }
+        by Chun Rapeepat, 2017 - 2018.
       </Container>
     )
   }
