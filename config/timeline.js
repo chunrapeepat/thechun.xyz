652e1e94ef244607cdf0624ef95df4d3ec55059e
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-10-02 14:56:46 +0700

    Make timeline config file.

---
+++ config/timeline.js
@@ -0,0 +1,202 @@
+export default [
+  {
+    year: "1999",
+    header: "Start Point",
+    content: "I was born in Bangkok, Thailand on May 9th, 1999",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2005",
+    header: "Studied Elementary School",
+    content: "Started studying at Somapa 2 School.",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2009",
+    header: "First Personal Computer",
+    content: "Got the first personal computer and I loved it until then.",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2010 - 2011",
+    header: "Made a YouTube Channel",
+    content:
+      "Making a YouTube channel with my brothers. This made me learn a lot about how to do effect on the video.",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2011",
+    header: "Middle School",
+    content: "Started studying primary school at Bodindecha 2 school.",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2012",
+    header: "Started Programming Experience",
+    content:
+      "I started first programming experience when I was 14 years old with PHP, Of course, I loved it. Then I started learning more in CSS, Java & Javascript",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2014 - 2015",
+    header: "Run a VPS Bussiness",
+    content:
+      "I had a pretty much strong knowledge about how to open Minecraft Server. I decided to make my own business named MineShare - A VPS services - with my own budget, and 6 months later this was shutdown.",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2015 - 2016",
+    header: "Web Developer Freelancer",
+    content:
+      "In 2015 - 2016, I am a freelancer in web development. I made a Facebook fanpage and made a lot of websites such as VPS service website, webshop (web application) and more. I make a lot of money from this.",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2016",
+    header: "NoobProgramming YouTube Channel",
+    content:
+      "Making my new YouTube channel named NoobProgramming. I created this for a people who just started in computer programming and doesn't know how to go.",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2016",
+    header: "Torkla Project by SCB Foundation",
+    content:
+      "Fortunately, My friend at my middle school invited me to run a project (Our darkest night Project) with him. In Tokla camp, I learned a lot from it. I learn how to do project management, UX, UI, I learned how to present and The most important is I met a variety of people who loved in what they did.",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2017",
+    header: "Go To College",
+    content: "Studied at Kasetsart University in Computer Science program.",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2017",
+    header: "NoobProgrammer Thailand Community",
+    content:
+      "I decided to create this group to make a young people who loves in programming more connected.",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2017",
+    header: "Organize Stupid Hackathon Thailand",
+    content:
+      "Organize a Stupid Hackathon event at NabLab Bangkok, Thailand. with theme: Make stuff that no one needs and make terrible ideas come true!",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2017",
+    header: "KU-NAVI Project",
+    content:
+      "KU-NAVI is the web application that can helps you find place, location & bus-line in college based on QRCode poster.",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2017",
+    header: "Internship at Acourse.io",
+    content:
+      "Software development internship traning at Acourse.io (e-learning online platform in cheap price), This made me learn a lot about Golang, Github and how to working with the team.",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2017",
+    header: "Teacher Assistant @CodeCampTH",
+    content:
+      "CodeCampTH (at Software Park) is a developer bootcamp that teaching web programming technologies & tools. I got an opportunity to work on this for 5 days and that earn me for 5,000 THB (approx 155$).",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2017",
+    header: "KU Grade Report Beautifier",
+    content:
+      "A grade report beautifier web-application for all Kasetsart University students. our goal is to make a report more beautiful, readable and simple than the old one. See this project on my Github :)",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2018",
+    header: "Young Webmaster Camp 15",
+    content:
+      "HNY 2018 with the greatest web camp. YWC is a webmaster camp that separates all candidates into 4 fields (programming, marketting, design, content). I was in programming field and working on #RARTY project.",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2018",
+    header: "Organize Junior Webmaster Camp #10",
+    content:
+      "Organize Junior Webmaster Camp: Nextstep Web Thai (JWCx) as Web Developer, Teacher in programming field and Techniques with 30+ friends from YWC.",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "2018",
+    header: "Attended Startup Thailand Hackathon",
+    content:
+      "Startup Thailand is the biggest startup event in Asia. so I decided to attend hackathon with the idea about the application to pre-order food from food-center and we call it `fastfood`.",
+
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "",
+    header: "Attended KU Startup Bootcamp #2",
+    content:
+      "KU Startup Bootcamp is a startup camp hosted by KU startup club. My team came with the idea of smart farming service and we call it `agriservice`, and finally, my team got the first prize :P",
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "",
+    header: "Attended Techsauce Global Summit 2018",
+    content:
+      "I'd have an opportunity to get into Techsauce which is the largest tech event in Asia at Centara Grand Bangkok. There are so many startup booths and talking stage which mostly talk about Blockchain & AI.",
+    tags: [],
+
+    ref: "",
+  },
+  {
+    year: "",
+    header: "Organize Stupid Hackathon Thailand #2",
+    content:
+      "Attended Stupid Hackathon #2 at LINK co-working space as both candidates and organizer. This times we reach over 100 candidates and get more sponsors than before. It was very fun and I made a lot of friends there.",
+
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "",
+    header: "Attended Young Startup Leader Camp",
+    content:
+      "Young startup leader camp by YEAH. Team building, Leadership, Startup Community & Nice Workshop",
+
+    tags: [],
+    ref: "",
+  },
+  {
+    year: "",
+    header: "Founded Abacuz Developer Group",
+    content:
+      "Abacuz Developer Group is the developer club at Kasetsart University. (Community, knowledge, Experience)",
+
+    tags: [],
+    ref: "",
+  },
+];
