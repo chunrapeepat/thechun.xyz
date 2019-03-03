27b0822f2158357df46c5f80b96e98524d788578
chunza2542 <chunza2542@hotmail.com>
2019-03-03 14:15:44 +0700

    Add more life-events

--- config/timeline.js
+++ config/timeline.js
@@ -199,4 +199,29 @@ export default [
     tags: [],
     ref: "",
   },
+  {
+    year: "2019",
+    header: "Organised CodePlearn",
+    content: "In 19 Jan 2019, After the child's day in Thailand, my friends and I organise CodePlearn (Code + Play + Learn) for Kid. I really fun, enjoys working with a lots of friends."
+  },
+  {
+    year: "2019",
+    header: "Air Pollution Content Project",
+    content: "The project is about air pollution problem in Thailand (PM2.5), So I decided to gather data from API, do a visualize thing and write a content on Medium to bring awareness to people about pollution problem."
+  },
+  {
+    year: "2019",
+    header: "Attended Chaingmai Hackatrain",
+    content: "Hackatrain is hackathon but hacking on a train ran from Bangkok to Changmai. I build a project with a theme Improve Travel Experience by using Big Data and visualize them in DataStudio for more insight. I won 2st prize for this project :P."
+  },
+  {
+    year: "2019",
+    header: "Attended Code in the dark #3",
+    content: "Code in the dark is a sub-event in Chaingmai Techfest. I attended the event instantly after I heard to event and then got into the final round. Unfortunately, something went wrong with the CSS and totally break site :("
+  },
+  {
+    year: "2019",
+    header: "Give a Talk at Talk 'n chip",
+    content: "Here is my first talk on the Tech event with around 20-30 attendees. I talk about my experience when I'm on high-school and make money as a Web developer (Freelance)."
+  },
 ];
